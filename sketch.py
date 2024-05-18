f88b20f2d6b286116989154bf75b424cc2ecc503
memonisha <75033521+memonisha@users.noreply.github.com>
2024-05-18 18:02:25 +0530

    Add files via upload

--- sketch.py
+++ sketch.py
@@ -9,7 +9,7 @@ def setup():
     c=createMovableCircle(250,200,20)
 
 def draw():
-    global a,b,c,yellow,blue,red
+    global a,b,c,yellow,blue,red, A,B,C
     background("black")
     drawTickAxes()
     
@@ -40,6 +40,26 @@ def draw():
     displayDistance(a,c,red,"red")
     
     triangleType()
+    displayAngle()
+    
+    fill("purple")
+    stroke("white")
+    strokeWeight(2)
+    textSize(10)
+    text(A,c.x,c.y)
+    
+    fill("teal")
+    stroke("white")
+    strokeWeight(2)
+    textSize(10)
+    text(B,a.x,a.y)
+    
+    fill("lime")
+    stroke("black")
+    strokeWeight(2)
+    textSize(10)
+    text(C,b.x,b.y)
+    
     
 
 def maketriangle(a,b,c): #functions with parameters
@@ -82,12 +102,46 @@ def triangleType():
     global yellow,blue,red
  
     if yellow == blue and yellow == red and blue == red:
-        triangleside = "Equilateral"
+        triangleside = "equilateral"
 
     elif yellow != blue and blue != red and red != yellow:
-        triangleside = "Scalene"
+        triangleside = "scalene"
     else:
-        triangleside = "Isosceles"
+        triangleside = "isosceles"
     textSize(30)
     fill("pink")
     text(f"Triangle is {triangleside}",100,650)
+
+def displayAngle():
+    global yellow, blue, red,A,B,C
+    a=yellow
+    b=blue
+    c=red
+    a2=a**2
+    b2=b**2
+    c2=c**2
+    #Python's Exception handling 
+    try:
+        A = round(acos((b2+c2-a2)/(2*b*c)))
+        B = round(acos((a2+c2-b2)/(2*a*c)))
+        C = 180 - A - B
+        print(A, B, C)
+        #B- teal one
+        #C-lime one
+        #A -purple one
+        if A == 90 or B == 90 or C == 90:
+            angle = "right"
+        elif A >90 or B > 90 or C > 90:
+            angle = "obtuse"
+        else:
+            angle = "acute"
+        textSize(30)
+        fill("orange")
+        text(f"Triangle is {angle} angled",100,600)
+    except:
+        fill("white")
+        textSize(40)
+        text("NOT A TRIANGLE ANYMORE",50,500)
+
+    
+    
